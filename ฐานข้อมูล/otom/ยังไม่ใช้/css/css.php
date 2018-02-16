736289ec49336976635ad1376ff23ce49f045760
lazyman00 <sky_of_light@hotmail.com>
2018-02-16 14:06:42 +0700

    3

--- ฐานข้อมูล/otom/ยังไม่ใช้/css/css.php
+++
@@ -1,4 +0,0 @@
-    <link href="../bootstrap/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    <link href="../bootstrap/vendor/metisMenu/metisMenu.min.css" rel="stylesheet">
-    <link href="../bootstrap/dist/css/sb-admin-2.css" rel="stylesheet">
-    <link href="../bootstrap/vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
--- ฐานข้อมูล/otom/ยังไม่ใช้/js/js.php
+++
@@ -1,4 +0,0 @@
-    <script src="../bootstrap/vendor/jquery/jquery.min.js"></script>
-    <script src="../bootstrap/vendor/bootstrap/js/bootstrap.min.js"></script>
-    <script src="../bootstrap/vendor/metisMenu/metisMenu.min.js"></script>
-    <script src="../bootstrap/dist/js/sb-admin-2.js"></script>
