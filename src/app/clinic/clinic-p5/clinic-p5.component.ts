0078434c53f14c96d6dc30969a3c64fe6cabebd0
anuchauru <anucha@uru.ac.th>
2018-02-17 22:36:43 +0700

    a19

--- src/app/clinic/clinic-p5/clinic-p5.component.ts
+++ src/app/clinic/clinic-p5/clinic-p5.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { ClinicserviceService } from '../clinicservice.service';
 
 @Component({
   selector: 'app-clinic-p5',
   templateUrl: './clinic-p5.component.html',
-  styleUrls: ['./clinic-p5.component.css']
+  styleUrls: ['./clinic-p5.component.css'],
+  providers: [ClinicserviceService]
 })
 export class ClinicP5Component implements OnInit {
-
-  constructor() { }
+  if_text = 'table';
+  data: any;
+  constructor(private cvl: ClinicserviceService) { }
 
   ngOnInit() {
+    this.cvl.get_tb_screening_cli().subscribe(
+      (txtdata) => this.data = txtdata,
+      (error) => alert(error),
+      () => {
+        console.log(this.data);
+      }
+    );
   }
 
 }
